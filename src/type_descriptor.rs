2774205bdf617c1258eebed6035485c29d6688ac
한진희 <0200134hjh@gmail.com>
2025-11-22 11:31:27 +0900

    Create type_descriptor.rs

---
+++ src/type_descriptor.rs
@@ -0,0 +1,25 @@
+pub fn rust_type_to_jvm(ty: &str) -> String {
+    match ty {
+        "i32" => "I".into(),
+        "i64" => "J".into(),
+        "u8"  => "B".into(),
+        "bool" => "Z".into(),
+        "()" => "V".into(),
+        "String" => "Ljava/lang/String;".into(),
+
+        // 리스트, 벡터 타입 매핑
+        t if t.starts_with("Vec<") => {
+            let inner = &t[4..t.len()-1];
+            format!("[{}", rust_type_to_jvm(inner))
+        }
+
+        // 참조 타입 (&T)
+        t if t.starts_with("&") => {
+            let inner = &t[1..];
+            rust_type_to_jvm(inner)
+        }
+
+        // 기타는 object 취급
+        other => format!("Lrust/{};", other.replace("::", "/")),
+    }
+}

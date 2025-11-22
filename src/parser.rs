9c856e8f3de4a8963d247945c2b9c4c8910deead
한진희 <0200134hjh@gmail.com>
2025-11-22 11:37:55 +0900

    Update parser.rs

--- src/parser.rs
+++ src/parser.rs
@@ -2,14 +2,16 @@
 pub struct AstFunction {
     pub name: String,
     pub params: Vec<String>,
+    pub ret_type: String,
     pub body: String,
 }
 
 pub fn parse(_src: &str) -> AstFunction {
-    
+    // 실제 Rust 파싱 대신 의미 기반 더미
     AstFunction {
-        name: "add".to_string(),
-        params: vec!["a".into(), "b".into()],
-        body: "a + b".to_string(),
+        name: "add".into(),
+        params: vec!["i32".into(), "i32".into()],
+        ret_type: "i32".into(),
+        body: "a + b".into(),
     }
 }

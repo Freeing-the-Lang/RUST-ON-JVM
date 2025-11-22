d232eb7d36c34f4a234037ea622a1d74ede4aacf
한진희 <0200134hjh@gmail.com>
2025-11-22 11:37:27 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,29 @@
 mod parser;
 mod semantic;
+mod ir;
 mod jvm_backend;
+mod classfile;
+mod constant_pool;
+mod type_descriptor;
 
 fn main() {
+    println!("[RUST-ON-JVM] Prototype Compiler");
+
     let src = r#"
         fn add(a: i32, b: i32) -> i32 {
             a + b
         }
     "#;
 
-    println!("RUST-ON-JVM Prototype\n");
-
     // 1. Parse
     let ast = parser::parse(src);
     println!("AST = {:#?}", ast);
 
-    // 2. Semantic meaning
-    let ir = semantic::analyze(ast);
+    // 2. Semantic → IR
+    let ir = semantic::analyze(&ast);
     println!("IR = {:#?}", ir);
 
-    // 3. Generate JVM bytecode
-    let bytecode = jvm_backend::generate(ir);
-    println!("JVM Bytecode = {:?}", bytecode);
+    // 3. JVM ClassFile 생성
+    jvm_backend::emit_classfile(&ir, "Add.class");
+    println!("Wrote Add.class");
 }

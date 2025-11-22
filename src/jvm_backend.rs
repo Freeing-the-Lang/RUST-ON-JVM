6eb9db7e6c2455a01a67d9563ff0ca51a0247828
한진희 <0200134hjh@gmail.com>
2025-11-22 11:45:03 +0900

    Update jvm_backend.rs

--- src/jvm_backend.rs
+++ src/jvm_backend.rs
@@ -1,31 +1,24 @@
-use crate::semantic::IRFunction;
+use crate::ir::IRFunction;
 use crate::classfile::ClassFile;
 
-pub fn generate_classfile(ir: IRFunction, path: &str) {
-    let bytecode = generate(ir);
-    let cf = ClassFile::new(bytecode.code);
-    cf.write_to_file(path);
+pub fn emit_classfile(ir: &IRFunction, path: &str) {
+    let bytecode = generate_bytecode(ir);
+    let cf = ClassFile::new(ir, bytecode);
+    cf.write(path);
 }
 
-
-#[derive(Debug)]
-pub struct Bytecode {
-    pub code: Vec<u8>,
-}
-
-pub fn generate(ir: IRFunction) -> Bytecode {
+pub fn generate_bytecode(ir: &IRFunction) -> Vec<u8> {
     let mut code = Vec::new();
 
-    // JVM opcode 예시 (실제와 다르게 단순화 버전)
-    for instr in ir.instr {
+    for instr in &ir.instr {
         match instr.as_str() {
-            "LOAD a" => code.push(0x15), // iload_0
-            "LOAD b" => code.push(0x16), // iload_1
-            "ADD"     => code.push(0x60), // iadd
-            "RETURN"  => code.push(0xac), // ireturn
+            "LOAD_0" => code.push(0x1a),
+            "LOAD_1" => code.push(0x1b),
+            "ADD"    => code.push(0x60),
+            "RETURN" => code.push(0xac),
             _ => {}
         }
     }
 
-    Bytecode { code }
+    code
 }
